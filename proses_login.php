0ba9377d7a77aee0f283456d101aafca6573c68c
imageconita <image19.fc@gmail.com>
2023-09-12 11:00:25 +0800

    upload project

---
+++ proses_login.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+$sql="SELECT * FROM user WHERE username ='$username' AND password = '$password'";
+$data = mysqli_query($conn, $sql);
+$result = mysqli_num_rows($data);
+
+if ($result===1) {
+        $_SESSION['username'] =$username;
+        header("location:index.php");
+} else {
+    echo "username dan password salah!";
+}
+
+?>
